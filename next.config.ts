82170d0517a4d39231085ec120a1f3a796dc56ec
vector-10 <duzieblaise10@gmail.com>
2025-08-21 00:09:13 +0100

    chore: added remote domains for CDN images served with unsplash

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com']
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
